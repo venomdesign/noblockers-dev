53bb692b0926b6db9bcf7cf4240d2f746a2735f8
venomdesign <chris.schweigart@venomdesign.com>
2025-07-29 23:00:02 -0400

    still trying to fix deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    base: '/',
+  base: '/', // do NOT put /noblockers/ for custom domains
+  site: 'https://noblockers.dev', // REQUIRED for correct asset resolving
+  outDir: 'dist',
 });
